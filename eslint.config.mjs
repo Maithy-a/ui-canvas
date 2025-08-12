98eb1eb057e6ba049161d629192a83bbda4fab6a
Maithy-a <maithyabon2@gmail.com>
2025-08-12 13:44:30 +0300

    disableeslint rules, to fix deploment error.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    rules: {
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
